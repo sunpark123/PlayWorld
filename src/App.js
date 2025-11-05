a7673c8d139f1ea52a963fbac4c3ee0d39e4992e
석현07 <92662236+sunpark123@users.noreply.github.com>
2025-11-05 09:57:15 +0900

    header set

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Lobby from './Lobby/Lobby'
+import Header from "./Header/Header";
 
 function App() {
 	
@@ -15,6 +16,7 @@ function AppContent() {
 	return (
 		<>	
 			<Routes>
+				<Route path="/header" element={<Header />} />
 				<Route path="/" element={<Lobby />} />
 			</Routes>
 		</>
---
+++ src/Header/Header.js
@@ -0,0 +1,18 @@
+import "./Header.css"
+
+function Header() {
+
+
+	return (
+		<>
+            <div className="header">
+                <div className="headerBox">
+                    <img src="puzzle.png"></img>
+                    <h1>Play World</h1>
+                </div>
+            </div>
+        </>
+	);
+}
+
+export default Header;
