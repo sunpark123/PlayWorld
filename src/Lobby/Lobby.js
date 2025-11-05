c9c92e4d56f2ecfc7bfd55a89e37449bef2764ae
석현07 <92662236+sunpark123@users.noreply.github.com>
2025-11-05 10:00:00 +0900

    test

---
+++ src/Lobby/Lobby.js
@@ -0,0 +1,18 @@
+
+
+function Lobby() {
+
+
+	return (
+		<>
+            <div className="header">
+                <div className="headerBox">
+                    <img src="puzzle.png"></img>
+                    <h1>Play Worlds</h1>
+                </div>
+            </div>
+        </>
+	);
+}
+
+export default Lobby;
